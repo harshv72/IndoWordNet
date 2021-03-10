51eaf819c621d71c22e8eccab4064f49725285a1
Sani Ranpariya <saniranpariya456@gmail.com>
2021-03-10 12:35:11 +0530

    synsetpart update

--- iwn/indoWordNet/views.py
+++ iwn/indoWordNet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from indoWordNet.models import TblAllWords,TblAllSynset
+from indoWordNet.models import TblAllWords,TblAllSynset,EnglishHindiIdMapping,EnglishSynsetData
+import base64
 # Create your views here.
 
 def index(request):
@@ -8,7 +9,6 @@ def index(request):
 
 def wordnet(request):
     word = str(request.GET.get('query'))
-    print(word)
     synset = TblAllWords.objects.filter(word = word)
     length = len(synset)
     wordList=[]
@@ -23,7 +23,17 @@ def wordnet(request):
         for j in synonuyms:
             s.append(str(j.word))
         l.append(s)
-        l.append(str(gloss.gloss))
+        data = gloss.gloss
+        data = data.decode('UTF-8')
+        data = data.split(':')
+        l.append(data)
+        try:
+            enId = EnglishHindiIdMapping.objects.using('region').get(hindi_id = str(i.synset_id))
+            glossEN = EnglishSynsetData.objects.using('region').filter(synset_id = str(enId.english_id))[0]
+            l.append(str(glossEN.gloss))
+        except EnglishHindiIdMapping.DoesNotExist:
+            l.append('English Linkage Not Available')
+        
         wordList.append(l)
     return render(request,'wordnet.html', {'query':word,'length':length,'wordList':wordList})
 
